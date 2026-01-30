51b97d2e36ae199fe8bee8c9552e15facee69f80
종훈 <jonghoon023@gmail.com>
2026-01-30 14:34:07 +0900

    프로젝트 추가

---
+++ backend/src/main/java/com/example/wordchain/WordchainApplication.java
@@ -0,0 +1,13 @@
+package com.example.wordchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordchainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordchainApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/wordchain/WordchainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wordchain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordchainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
